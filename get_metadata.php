2b3d28decf6723d7bb1340a731dea1c7f2338bdb
fouedb-support <122614826+fouedb-support@users.noreply.github.com>
2024-05-04 22:29:12 +0100

    Update get_metadata.php

--- get_metadata.php
+++ get_metadata.php
@@ -1,6 +1,4 @@
 <?php
-// Simuler la récupération des métadonnées depuis la source audio
 $metadata = 'Now Playing: Song Title - Artist Name';
-// Retourner les métadonnées au format JSON pour la manipulation côté client
 echo json_encode($metadata);
 ?>
